c94f4227210f4dd0cdb105917d34aebbd3f5cddb
Matthias Riegler <riegler.matthias2@gmail.com>
2018-06-26 20:10:45 +0200

    Moved to in memory database until we decide where to store the db

--- racecontrol/config.py
+++ racecontrol/config.py
@@ -1,10 +1,8 @@
 # Temporary solution!!!
-import os
 
 INITIAL_REDIRECT = "/core"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
 SECRET_KEY = 00000000000000000000000000000000
 SECRET_KEY = 'racecontroll-webui@development'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' \
-                          + os.path.join(os.getcwd(), '../db-dev.sqlite3')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
